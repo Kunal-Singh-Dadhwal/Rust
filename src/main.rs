10f8c978e7884cb73bd47f8a65eb78d7b36dd1f1
Kunal Singh Dadhwal <155142500+Kunal-Singh-Dadhwal@users.noreply.github.com>
2024-10-01 23:40:30 +0530

    Functions

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,32 @@ fn main() {
     num = 10;
     //    println!("The number stored in num is {}", num);
 
+    /*
+       &str - Fixed length string
+       String - Dynamic Length String - Heap Allocated
+    */
+
     let sentence: &str = "Hi, Rust lets learn you";
-    let secondsent: String = String::from("Hi, Rust lets learn you");
+    let mut secondsent: String = String::from("Hi, Rust lets learn you");
+    secondsent.push_str(". This is the pushed string");
     println!(
         "The sentence is \"{}\"\nThe second sentence is \"{}\"",
         sentence, secondsent
     );
+
+    let emp_info: (&str, u8) = ("Ramesh", 50);
+
+    // let emp_name = emp_info.0;
+    // let emp_age = emp_info.1;
+
+    let (emp_name, emp_age) = emp_info;
+    println!(
+        "The employee name is {} and the employee age is {}",
+        emp_name, emp_age
+    );
+    print_val();
+}
+
+fn print_val() {
+    println!("Hello world");
 }
