c3fca0a30e134092bc3b1d1f8632fee52e360dd2
Kunal Singh Dadhwal <155142500+Kunal-Singh-Dadhwal@users.noreply.github.com>
2024-10-30 17:37:48 +0530

    Lifetimes

--- src/lifetime.rs
+++ src/lifetime.rs
@@ -27,6 +27,12 @@
 
 // we use this generic lifetime can be also <'a>
 // it can be named as a variable
+// The lifetime of the return type is the union of the lifetime of the 
+// first and the seconde parameter
+// 
+//
+// so the lifetime of longest_str is same as str2 it ends after } 
+// Or in simple words the parameter with shortest lifetime will be the lifetime of the return 
 fn longest<'t>(a: &'t str, b: &'t str) -> &'t str {
     if a.len() > b.len() {
         a
@@ -43,11 +49,11 @@ fn main(){
     let str1 = String::from("small");
     let str2 = String::from("longer");
     // here also lifetime rule comes here
-    longest_str = longest(&str1, &str2);
-    // {
-    //     let str2 = String::from("longer");
-    //     longest_str = longest(&str1,&str2);
-    // }
+    // longest_str = longest(&str1, &str2);
+    {
+        let str2 = String::from("longer");
+        longest_str = longest(&str1,&str2);
+    }
     println!("{}", longest_str);
 }
 
